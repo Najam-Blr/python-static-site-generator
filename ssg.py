75a6209c17015fb21542916f2abca9cd3c1922fa
Najamunnisa Begum <64567091+Najam-Blr@users.noreply.github.com>
2021-04-22 16:32:19 -0400

    Changes for parser class

--- ssg.py
+++ ssg.py
@@ -1,9 +1,12 @@
 import typer
+
+import ssg.parsers
 from ssg.site import Site
+from ssg.parsers import Parser, ResourceParser
 
 
 def main(source="content", dest="dist"):
-    config = {"source": source, "dest": dest}
+    config = {"source": source, "dest": dest, "parsers": [ssg.parsers.ResourceParser()]}
     Site(**config).build()
 
 
